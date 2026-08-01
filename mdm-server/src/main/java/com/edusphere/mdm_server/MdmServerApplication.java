1e7ad2cdaa33705496c282a51a6846e1ae5c6901
VuTriDung1123 <dungvutri25@gmail.com>
2026-08-01 22:18:08 +0700

    Init basecode: Android Agent, Backend server and web dashboard

---
+++ mdm-server/src/main/java/com/edusphere/mdm_server/MdmServerApplication.java
@@ -0,0 +1,13 @@
+package com.edusphere.mdm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmServerApplication.class, args);
+	}
+
+}
---
+++ mdm-server/src/test/java/com/edusphere/mdm_server/MdmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusphere.mdm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
